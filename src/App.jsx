6c7acd0f7d316cbd7cba64202f84ed0e0a3e423f
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2023-10-08 16:33:35 +0200

    Create basic app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+
 export default function App() {
     return (
-        <div>
-            TEST
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route element={<Layout />}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                </Route>
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
