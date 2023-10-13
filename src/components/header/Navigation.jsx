72de5b660867ac7ee85ddb01a87b258902d6c4ea
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2023-10-13 17:05:05 +0200

    Add functionality of closing the navigation menu by clicking outside of the component

--- src/components/header/Navigation.jsx
+++ src/components/header/Navigation.jsx
@@ -1,16 +1,27 @@
+import { useRef } from 'react';
+import useClickOutside from '../../hooks/useClickOutside';
 import NavigationItem from './NavigationItem';
 import { AiOutlineClose } from 'react-icons/ai';
 import './navigation.scss';
 
 export default function Navigation({ isNavigationOpen, onSetIsNavigationOpen }) {
+    const navigationRef = useRef(null);
+
+    useClickOutside(navigationRef, () => {
+        onSetIsNavigationOpen(false);
+    })
+
     return (
-        <nav className={`navigation ${isNavigationOpen ? 'navigation--visible' : ''}`}>
+        <nav
+            ref={navigationRef}
+            className={`navigation ${isNavigationOpen ? 'navigation--visible' : ''}`}
+        >
             <div className='navigation__close-button-container'>
                 <button
                     className='navigation__close-button'
                     onClick={() => onSetIsNavigationOpen(false)}
                 >
-                    <AiOutlineClose size={'32px'} className='navigation__close-icon'/>
+                    <AiOutlineClose size={'34px'} className='navigation__close-icon'/>
                 </button>
             </div>
             <ul className="navigation__list">
