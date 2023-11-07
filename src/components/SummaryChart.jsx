b91b3130f81b99a94671fc9fd9ba7043e1286b0e
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2023-11-07 19:36:27 +0100

    Update styles for summary chart component

--- src/components/SummaryChart.jsx
+++ src/components/SummaryChart.jsx
@@ -1,8 +1,15 @@
+import BasicProgressBar from './BasicProgressBar';
+import CircleProgressBar from './CircleProgressBar';
 import './summaryChart.scss';
 
 export default function SummaryChart({ heading, dayValues, userLimitValues }) {
     const { calories, proteins, fats, carbohydrates } = dayValues;
     const { caloriesLimit, proteinsLimit, fatsLimit, carbohydratesLimit } = userLimitValues;
+
+    const aboveCaloriesLimit = (calories - caloriesLimit) > 0;
+    const aboveProteinsLimit = (proteins - proteinsLimit) > 0;
+    const aboveFatsLimit = (fats - fatsLimit) > 0;
+    const aboveCarbohydratesLimit = (carbohydrates - carbohydratesLimit) > 0;
 
     return (
         <section className="summary-chart">
@@ -10,18 +17,42 @@ export default function SummaryChart({ heading, dayValues, userLimitValues }) {
                 {heading}
             </h2>
             <div className='summary-chart__chart-container'>
-                <div className='summary-chart__kcal-container'>
-                    {`${calories}/${caloriesLimit}`}
+                <div className='summary-chart__cal-container'>
+                    <CircleProgressBar
+                        value={calories}
+                        limit={caloriesLimit}
+                        color={aboveCaloriesLimit ? 'above-limit-progress-color' : 'calories-progress-color'}
+                    >
+                        {`${calories}/${caloriesLimit} calories`}
+                    </CircleProgressBar>
                 </div>
                 <div className='summary-chart__nutritions-container'>
                     <div className='summary-chart__proteins-container'>
-                        {`${proteins}/${proteinsLimit}`}
+                        <BasicProgressBar
+                            value={proteins}
+                            limit={proteinsLimit}
+                            color={aboveProteinsLimit ? 'above-limit-progress-color' : 'proteins-progress-color'}
+                        >
+                            {`${proteins}g/${proteinsLimit}g`}
+                        </BasicProgressBar>
                     </div>
                     <div className='summary-chart__fats-container'>
-                        {`${fats}/${fatsLimit}`}
+                        <BasicProgressBar
+                            value={fats}
+                            limit={fatsLimit}
+                            color={aboveFatsLimit ? 'above-limit-progress-color' : 'fats-progress-color'}
+                        >
+                            {`${fats}g/${fatsLimit}g`}
+                        </BasicProgressBar>
                     </div>
                     <div className='summary-chart__carbohydrates-container'>
-                        {`${carbohydrates}/${carbohydratesLimit}`}
+                        <BasicProgressBar
+                            value={carbohydrates}
+                            limit={carbohydratesLimit}
+                            color={aboveCarbohydratesLimit ? 'above-limit-progress-color' : 'carbohydrates-progress-color'}
+                        >
+                            {`${carbohydrates}g/${carbohydratesLimit}g`}
+                        </BasicProgressBar>
                     </div>
                 </div>
             </div>
