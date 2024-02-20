8e803fb3f9a5c1124f30ac64dfb73f8ac221d337
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2024-02-20 19:55:55 +0100

    Create constants file with data

---
+++ src/data/constants.js
@@ -0,0 +1,42 @@
+export const meals = [
+    {
+        id: 1232,
+        name: 'Breakfast',
+        products: [
+            {
+                id: 34234,
+                name: 'Bread',
+                quantity: '4 slices',
+                calories: 300,
+                proteins: 10,
+                fats: 1,
+                carbohydrates: 58,
+            },
+            {
+                id: 7836,
+                name: 'Butter',
+                quantity: '12g',
+                calories: 100,
+                proteins: 1,
+                fats: 10,
+                carbohydrates: 0,
+            },
+            {
+                id: 65334,
+                name: 'Ham',
+                quantity: '50g',
+                calories: 115,
+                proteins: 16,
+                fats: 8,
+                carbohydrates: 1,
+            },
+        ],
+    },
+];
+
+export const userLimitValues = {
+    caloriesLimit: 2500,
+    proteinsLimit: 100,
+    fatsLimit: 75,
+    carbohydratesLimit: 340,
+}
