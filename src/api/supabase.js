615d298585851d529dfdaa9c2300712e1ae6e179
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2024-02-21 18:09:55 +0100

    Remove environmental variables

--- src/api/supabase.js
+++ src/api/supabase.js
@@ -1,14 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://qxocswviuhfdtktvbsil.supabase.co';
-
-let supabaseKey = null;
-if (process.env.NODE_ENV === 'production') {
-    supabaseKey = process.env.VITE_API_KEY;
-} else {
-    supabaseKey = import.meta.env.VITE_API_KEY;
-}
-
+// This key is safe to use in a browser if Row Level Security for tables is enabled and policies are configured.
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4b2Nzd3ZpdWhmZHRrdHZic2lsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg0NTc0MTcsImV4cCI6MjAyNDAzMzQxN30.kNsVJosRvarMyn3ufKDf9Y03Z6964_AE1Gm71eyh0OI';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
