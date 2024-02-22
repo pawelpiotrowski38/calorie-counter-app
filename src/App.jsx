9261087fef304e97fb897d3806f1acd0805950b7
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2024-02-22 20:12:56 +0100

    Configure toast container

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ToastContainer } from 'react-toastify';
 import AppLayout from './ui/AppLayout';
 import Home from './pages/Home';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
+import 'react-toastify/dist/ReactToastify.css';
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -26,6 +28,17 @@ export default function App() {
                     <Route path="*" element={<NotFound />} />
                 </Routes>
             </BrowserRouter>
+            <ToastContainer
+                toastClassName='toast-wrapper'
+                autoClose={2500}
+                pauseOnHover={false}
+                hideProgressBar
+                closeButton={false}
+                theme='dark'
+                style={{
+                    fontSize: '0.9375rem',
+                }}
+            />
             <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
     )
