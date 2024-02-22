023e65a4adf24bb18bc82bc02981ec3509ee2577
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2024-02-22 18:03:05 +0100

    Use react query to fetch meals data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import { getMeals } from '../api/apiMeals';
 import { defaultMeals, userLimitValues } from '../data/constants';
 import { formatTodayDate } from '../utils/dateUtils';
@@ -7,16 +8,16 @@ import SummaryChart from '../features/summaries/SummaryChart';
 import MealNutritions from '../features/meals/MealNutritions';
 import MealsList from '../features/meals/MealsList';
 import Meal from '../features/meals/Meal';
+import Spinner from '../ui/Spinner';
 import './home.scss';
 
 export default function Home() {
-    const [meals, setMeals] = useState([]);
     const [selectedDate, setSelectedDate] = useState(new Date());
 
-    useEffect(function () {
-        getMeals(formatTodayDate(selectedDate))
-            .then((data) => setMeals(data));
-    }, [selectedDate]);
+    const { isLoading, data: meals, error, } = useQuery({
+        queryKey: ['meals', formatTodayDate(selectedDate)],
+        queryFn: () => getMeals(formatTodayDate(selectedDate)),
+    });
 
     return (
         <div className='home'>
@@ -26,33 +27,41 @@ export default function Home() {
                     onSetSelectedDate={setSelectedDate}
                 />
             </div>
-            <div className='home__summary-chart-container'>
-                <SummaryChart
-                    heading={'Daily summary'}
-                    meals={meals || []}
-                    userLimitValues={userLimitValues}
-                />
-            </div>
-            <div className='home__meals-container'>
-                <MealNutritions
-                    calories={''}
-                    proteins={'P'}
-                    fats={'F'}
-                    carbohydrates={'C'}
-                />
-                <MealsList>                                     
-                    {defaultMeals.map((defaultMeal) => {
-                        const matchingMeal = meals.find((meal) => meal.meal_type === defaultMeal.meal_type);
-                        return (
-                            <Meal
-                                key={defaultMeal.meal_type}
-                                meal={matchingMeal || defaultMeal}
-                                selectedDate={selectedDate}
-                            />
-                        )
-                    })}
-                </MealsList>
-            </div>
+            {isLoading ? (
+                <div className='home__loading-container'>
+                    <Spinner />
+                </div>
+            ): (
+                <>
+                    <div className='home__summary-chart-container'>
+                        <SummaryChart
+                            heading={'Daily summary'}
+                            meals={meals || []}
+                            userLimitValues={userLimitValues}
+                        />
+                    </div>
+                    <div className='home__meals-container'>
+                        <MealNutritions
+                            calories={''}
+                            proteins={'P'}
+                            fats={'F'}
+                            carbohydrates={'C'}
+                        />
+                        <MealsList>                                     
+                            {defaultMeals.map((defaultMeal) => {
+                                const matchingMeal = meals.find((meal) => meal.meal_type === defaultMeal.meal_type);
+                                return (
+                                    <Meal
+                                        key={defaultMeal.meal_type}
+                                        meal={matchingMeal || defaultMeal}
+                                        selectedDate={selectedDate}
+                                    />
+                                )
+                            })}
+                        </MealsList>
+                    </div>
+                </>
+            )}
         </div>
     )
 }
