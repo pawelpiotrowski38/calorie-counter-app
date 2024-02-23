0a30b23e52753b660be1f34f42e821292df18328
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2024-02-23 21:48:11 +0100

    Create api function for adding meal item

--- src/api/apiMeals.js
+++ src/api/apiMeals.js
@@ -31,6 +31,68 @@ export async function deleteMeal(id) {
     return data;
 }
 
+export async function addMealItem(mealItem) {
+    const doesMealExist = mealItem.mealId > 0;
+    
+    let newMealId = 0;
+
+    if (!doesMealExist) {
+        const { data, error } = await supabase
+            .from('meals')
+            .insert([
+                { date: mealItem.mealDate, meal_type: mealItem.mealType },
+            ])
+            .select('id');
+
+        if (error) {
+            console.error(error);
+            throw new Error('Meal could not be added');
+        }
+
+        newMealId = data[0].id;
+    }
+
+    const newMealItemMealId = newMealId || mealItem.mealId;
+
+    let caloriesValue = 0;
+    let proteinsValue = 0;
+    let fatsValue = 0;
+    let carbohydratesValue = 0;
+
+    mealItem.nutrients.forEach((nutrient) => {
+        if (nutrient.nutrientId === 1008) {
+            caloriesValue = nutrient.value;
+        } else if (nutrient.nutrientId === 1003) {
+            proteinsValue = nutrient.value;
+        } else if (nutrient.nutrientId === 1004) {
+            fatsValue = nutrient.value;
+        } else if (nutrient.nutrientId === 1005) {
+            carbohydratesValue = nutrient.value;
+        }
+    });
+
+    const { data, error } = await supabase
+        .from('meal_items')
+        .insert([
+            {
+                meal_id: newMealItemMealId,
+                name: mealItem.name,
+                quantity: 100,
+                calories: caloriesValue,
+                proteins: proteinsValue,
+                fats: fatsValue,
+                carbohydrates: carbohydratesValue,
+            },
+        ]);
+    
+    if (error) {
+        console.error(error);
+        throw new Error('Meal item could not be added');
+    }
+
+    return data;
+}
+
 export async function deleteMealItem(id) {
     const { data, error } = await supabase
         .from('meal_items')
