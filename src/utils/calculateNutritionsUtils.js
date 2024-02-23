822a07ba3a9161b5e4cfc4204647fae9ff05eea8
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2024-02-23 21:44:56 +0100

    Round sum in calculating functions

--- src/utils/calculateNutritionsUtils.js
+++ src/utils/calculateNutritionsUtils.js
@@ -1,15 +1,19 @@
-export const calculateDayNutritionSum = (meals, nutrition) => {
-    return meals.reduce((acc, currentObject) => {
+export const calculateDayNutritionSum = (meals, nutrition, decimalPlaces = 1) => {
+    const sum = meals.reduce((acc, currentObject) => {
         const mealItems = currentObject.meal_items;
         const nestedValue = mealItems.reduce((nestedAcc, nestedObject) => {
             return nestedAcc + nestedObject[nutrition];
         }, 0);
         return acc + nestedValue;
     }, 0);
+
+    return parseFloat(sum.toFixed(decimalPlaces));
 }
 
-export function calculateMealNutritionSum(meal, nutrition) {
-    return meal.meal_items.reduce((acc, currentValue) => {
+export function calculateMealNutritionSum(meal, nutrition, decimalPlaces = 1) {
+    const sum = meal.meal_items.reduce((acc, currentValue) => {
         return acc + currentValue[nutrition];
     }, 0);
+
+    return parseFloat(sum.toFixed(decimalPlaces));
 }
