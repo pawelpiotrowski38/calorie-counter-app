11fa48a88681295a85b149587cf064be6d9fc355
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2024-02-25 20:03:20 +0100

    Add function to calculte product nutrients

--- src/utils/calculateNutritionsUtils.js
+++ src/utils/calculateNutritionsUtils.js
@@ -17,3 +17,33 @@ export function calculateMealNutritionSum(meal, nutrition, decimalPlaces = 1) {
 
     return parseFloat(sum.toFixed(decimalPlaces));
 }
+
+export function calculateProductNutrition(nutrients, servingSize) {
+    let caloriesValue, caloriesServingValue = 0;
+    let proteinsValue, proteinsServingValue = 0;
+    let fatsValue, fatsServingValue = 0;
+    let carbohydratesValue, carbohydratesServingValue = 0;
+
+    nutrients.forEach((nutrient) => {
+        if (nutrient.nutrientId === 1008) {
+            caloriesValue = nutrient.value;
+            caloriesServingValue = Math.round(servingSize/100 * nutrient.value);
+        } else if (nutrient.nutrientId === 1003) {
+            proteinsValue = nutrient.value;
+            proteinsServingValue = parseFloat((servingSize/100 * nutrient.value).toFixed(2));
+        } else if (nutrient.nutrientId === 1004) {
+            fatsValue = nutrient.value;
+            fatsServingValue = parseFloat((servingSize/100 * nutrient.value).toFixed(2));
+        } else if (nutrient.nutrientId === 1005) {
+            carbohydratesValue = nutrient.value;
+            carbohydratesServingValue = parseFloat((servingSize/100 * nutrient.value).toFixed(2));
+        }
+    });
+
+    return {
+        calories: { name: 'Energy (Cal)', value: caloriesValue, servingValue: caloriesServingValue, },
+        proteins: { name: 'Proteins (g)', value: proteinsValue, servingValue: proteinsServingValue, },
+        fats: { name: 'Fats (g)', value: fatsValue, servingValue: fatsServingValue, },
+        carbohydrates: { name: 'Carbohydrates (g)', value: carbohydratesValue, servingValue: carbohydratesServingValue, },
+    };
+}
