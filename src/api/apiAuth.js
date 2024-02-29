c70e3adc02ba50a95301bfd679eb3f8568b00d2b
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2024-02-29 19:17:25 +0100

    Add function to get current logged in user data

--- src/api/apiAuth.js
+++ src/api/apiAuth.js
@@ -12,3 +12,19 @@ export async function login({ email, password }) {
 
     return data;
 }
+
+export async function getCurrentUser() {
+    const { data } = await supabase.auth.getSession();
+
+    if(!data.session) {
+        return null;
+    }
+
+    const { data: { user }, error } = await supabase.auth.getUser();
+
+    if (error) {
+        throw new Error(error.message);
+    }
+
+    return user;
+}
