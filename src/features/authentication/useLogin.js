25e4560198c8be46790bba9e335ab3f01ac2f732
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2024-03-03 18:17:05 +0100

    Fix redirecting to login page after logging in

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,20 +1,22 @@
 import { useNavigate } from 'react-router-dom';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { login as loginApi } from '../../api/apiAuth';
 import { toast } from 'react-toastify';
 
 export function useLogin() {
     const navigate = useNavigate();
+    const queryClient = useQueryClient();
     
-    const { isPending: isLogging, mutate: login } = useMutation({
+    const { isPending: isLoggingIn, mutate: login } = useMutation({
         mutationFn: ({ email, password }) => loginApi({ email, password }),
-        onSuccess: () => {
-            navigate('/');
+        onSuccess: (user) => {
+            queryClient.setQueryData(['user'], user.user)
+            navigate('/', { replace: true });
         },
         onError: (error) => {
             toast.error(error.message);
         },
     });
 
-    return { isLogging, login };
+    return { isLoggingIn, login };
 }
