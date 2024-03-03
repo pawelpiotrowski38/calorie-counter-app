c14f110f77283294c259fd7355e36fb188607ead
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2024-03-03 18:10:33 +0100

    Update logo

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom';
+import './logo.scss';
+
 export default function Logo() {
     return (
-        <div className="logo">
-            <img className="logo__image" src="asd" alt="logo" />
+        <div className='logo'>
+            <Link to='/diary'>
+                <img className='logo__image' src='images/logo.png' alt='Calorie Counter' />
+            </Link>
         </div>
     )
 }
