61dc79fafc5073a33b3ca7578e59b551133df905
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2024-03-03 18:09:27 +0100

    Modify button component to take link prop

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,15 +1,41 @@
+import { Link } from 'react-router-dom';
 import './button.scss';
 
-export default function Button({ onClick, disabled, width, children }) {
+export default function Button({ onClick, linkTo, disabled, width, children }) {
     const style = {
         width: width,
+    }
+
+    if (linkTo) {
+        return (
+            <Link
+                className='button'
+                to={linkTo}
+                onClick={onClick}
+                style={style}
+            >
+                {children}
+            </Link>
+        )
+    }
+
+    if (onClick) {
+        return (
+            <button
+                className='button'
+                disabled={disabled}
+                onClick={onClick}
+                style={style}
+            >
+                {children}
+            </button>
+        )
     }
     
     return (
         <button
             className='button'
             disabled={disabled}
-            onClick={onClick}
             style={style}
         >
             {children}
@@ -19,6 +45,7 @@ export default function Button({ onClick, disabled, width, children }) {
 
 Button.defaultProps = {
     onClick: null,
+    linkTo: null,
     disabled: false,
     width: '100%',
 }
