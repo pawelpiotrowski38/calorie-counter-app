529f6cb3f86345b56b586baa1175f55d77d0258e
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2024-03-03 18:14:21 +0100

    Create diary page

---
+++ src/pages/Diary.jsx
@@ -0,0 +1,10 @@
+import Meals from '../features/meals/Meals';
+import './diary.scss';
+
+export default function Diary() {
+    return (
+        <div className='diary'>
+            <Meals />
+        </div> 
+    )
+}
