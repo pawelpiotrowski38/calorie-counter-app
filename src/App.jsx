7ec54139976e4b088df5d1dac63d85095267b343
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2024-03-03 18:20:32 +0100

    Protect diary page route from unauthorized access

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ToastContainer } from 'react-toastify';
 import AppLayout from './ui/AppLayout';
 import ProtectedRoute from './ui/ProtectedRoute';
 import Home from './pages/Home';
-import About from './pages/About';
+import Diary from './pages/Diary';
 import Login from './pages/Login';
+import Register from './pages/Register';
 import NotFound from './pages/NotFound';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -23,16 +24,21 @@ export default function App() {
         <QueryClientProvider client={queryClient}>
             <BrowserRouter>
                 <Routes>
-                    <Route element={
-                        <ProtectedRoute>
-                            <AppLayout />
-                        </ProtectedRoute>
-                    }>
-                        <Route path='/' element={<Home />} />
-                        <Route path='/about' element={<About />} />
+                    <Route element={<AppLayout />}>
+                        <Route path='/' element={<Navigate to='/diary' />} />
+                        <Route path='/home' element={<Home />} />
+                        <Route path='/login' element={<Login />} />
+                        <Route path='/register' element={<Register />} />
+                        <Route
+                            path='/diary'
+                            element={
+                                <ProtectedRoute>
+                                    <Diary />
+                                </ProtectedRoute>
+                            }
+                        />
+                        <Route path="*" element={<NotFound />} />
                     </Route>
-                    <Route path='/login' element={<Login />} />
-                    <Route path="*" element={<NotFound />} />
                 </Routes>
             </BrowserRouter>
             <ToastContainer
--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,24 +1,17 @@
-import { useNavigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { useUser } from '../features/authentication/useUser';
-import { useEffect } from 'react';
 import Spinner from './Spinner';
 
 export default function ProtectedRoute({ children }) {
-    const navigate = useNavigate();
-
     const { isGettingUser, isAuthenticated } = useUser();
-
-    useEffect(function() {
-        if (!isAuthenticated && !isGettingUser) {
-            navigate('/login');
-        }
-    }, [isAuthenticated, isGettingUser, navigate]);
 
     if (isGettingUser) {
         return <Spinner />
     }
 
-    if (isAuthenticated) {
-        return children;
+    if (!isAuthenticated) {
+        return <Navigate to='/home' replace />;
     }
+
+    return children;
 }
