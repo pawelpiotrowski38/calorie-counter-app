23ea860627df6522464d8c7a34393e1403a8ee84
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2024-03-04 18:03:28 +0100

    Render modal with meal item edit component

--- src/features/meals/MealProductsItem.jsx
+++ src/features/meals/MealProductsItem.jsx
@@ -1,11 +1,31 @@
+import { useState } from 'react';
 import { RiEdit2Line } from 'react-icons/ri';
 import { RiDeleteBin2Line } from 'react-icons/ri';
+import { useEditMealItem } from './useEditMealItem';
 import MealNutritions from './MealNutritions';
+import MealProductsItemEdit from './MealProductsItemEdit';
+import Modal from '../../ui/Modal';
 import './mealProductsItem.scss';
 
-export default function MealProductsItem({ product, isDeletingMealItem, onDeleteMealItem }) {
+export default function MealProductsItem({ product, isDeletingMealItem, onDeleteMealItem, selectedDate }) {
+    const [isEditOpen, setIsEditOpen] = useState(false);
+
+    const { isEditingMealItem, editMealItem } = useEditMealItem(selectedDate);
+
     return (
         <li className='meal-products-item'>
+            {isEditOpen && (
+                <Modal isOpen={isEditOpen}>
+                    <MealProductsItemEdit
+                        productId={product.id}
+                        quantity={product.quantity}
+                        isEditingMealItem={isEditingMealItem}
+                        onEditMealItem={editMealItem}
+                        onSetIsEditOpen={setIsEditOpen}
+                        selectedDate={selectedDate}
+                    />
+                </Modal>
+            )}
             <div className='meal-products-item__info-container'>
                 <h3 className='meal-products-item__name'>
                     {product.name}
@@ -23,6 +43,7 @@ export default function MealProductsItem({ product, isDeletingMealItem, onDelete
             <div className='meal-products-item__buttons-container'>
                 <button
                     className='meal-products-item__button meal-products-item__button--edit'
+                    onClick={() => setIsEditOpen(!isEditOpen)}
                     disabled={isDeletingMealItem}
                 >
                     <RiEdit2Line />
